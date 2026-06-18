96dca723cbffc005c5ff9dde1cc665ed32a53b4d
SayanH-BIT <sayanhaldar05@gmail.com>
2026-06-18 19:24:36 +0530

    updated cors policy

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ const app = express();
 // cors permission
 app.use(cors({
     origin: [process.env.CLIENT_URL, 'http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true
 }));
 
