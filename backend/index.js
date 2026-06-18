24a44d819de66289f898b30612cf3f6a9d8e57d2
SayanH-BIT <sayanhaldar05@gmail.com>
2026-06-18 20:13:04 +0530

    Reorder CORS middleware and add global options handler

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,9 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
+app.options('*', cors()); 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // db connection
 connectDB();
